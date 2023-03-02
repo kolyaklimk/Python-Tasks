94644b4ba5058a357c7b760f2dbd85347ba534ec
kolyaklimk <kolyaklimk@gmail.com>
2023-03-02 10:24:49 +0300

    add function that given even numbers of list

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -13,6 +13,10 @@ def calculator(num1, num2, operation):
     return "Error"
 
 
+def even_list(list_of_nums):
+    return [even_num for even_num in list_of_nums if even_num % 2 == 0]
+
+
 print(calculator(32, 51, "add"))
 print(calculator(143, 252, "sub"))
 print(calculator(623, 23, "mult"))
