48ee1eafc7baaea486858df589bb2acfcdae1d2e
kolyaklimk <kolyaklimk@gmail.com>
2023-03-26 17:08:28 +0300

    Create def amount_of_sentences and changed input text

--- Lab2/Task1/Task1.py
+++ Lab2/Task1/Task1.py
@@ -1,4 +1,13 @@
-text ='This is a text  with   some   separators  and     ' \
-      'multiple     sentences     .   Foo  bar  baz ...  ' \
-      'abbreviations  can  also  appear  a1b2c3  1234  . ' \
-      'Foo bar baz  hello world  hello  world!  Hi there  .Hi...there  .'
+import re
+
+
+def amount_of_sentences(text):
+    return len(re.findall(r'[^.]{2}[.]\s', text)) + len(re.findall(r'[.]{3}\s', text))
+
+
+input_text = 'This is a text with some separators  and ' \
+             'multiple sentences. Foo bar baz... ' \
+             'abbreviations can also appear a1b2c3 1234. ' \
+             'Foo bar baz hello world hello world. Hi there. Hi... there. '
+
+print(amount_of_sentences(input_text))
