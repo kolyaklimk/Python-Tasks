d00fc8508a9122ada4cd64cc7ec6d47355e85691
kolyaklimk <kolyaklimk@gmail.com>
2023-03-26 18:52:27 +0300

    Create def average_lenght_of_the_word

--- Lab2/Task1/Task1.py
+++ Lab2/Task1/Task1.py
@@ -24,6 +24,19 @@ def average_lenght_of_the_sentence(text):
     return count_char / count_sentences
 
 
+def average_lenght_of_the_word(text):
+    words = re.findall(r'(?=\w).*?(?=\W)', text)
+    count_words = len(words)
+    count_char = 0
+
+    for w in words:
+        if len(w) == len(re.findall(r'\d', w)):
+            continue
+        count_char += len(w)
+
+    return count_char / count_words
+
+
 input_text = 'This is a text 123 a32 with some separators, and ' \
              'multiple sentences. Foo bar baz... ' \
              'Abbreviations can also appear a1b2c3 1234. ' \
@@ -33,3 +46,4 @@ input_text = 'This is a text 123 a32 with some separators, and ' \
 print(amount_of_sentences(' ' + input_text + ' '))
 print(amount_of_non_declarative_sentences(' ' + input_text + ' '))
 print(average_lenght_of_the_sentence(' ' + input_text + ' '))
+print(average_lenght_of_the_word(' ' + input_text + ' '))
