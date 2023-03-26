fbdf4a6dd1118c58f8afe56ff3886ef2476e0556
kolyaklimk <kolyaklimk@gmail.com>
2023-03-26 15:54:14 +0300

    Create Lab2 folder and Task1.py with example for input

---
+++ Lab2/Task1/Task1.py
@@ -0,0 +1,4 @@
+text ='This is a text  with   some   separators  and     ' \
+      'multiple     sentences     .   Foo  bar  baz ...  ' \
+      'abbreviations  can  also  appear  a1b2c3  1234  . ' \
+      'Foo bar baz  hello world  hello  world!  Hi there  .Hi...there  .'
