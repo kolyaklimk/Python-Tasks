55e52947f43d5c4e19c93df9c581ca7c1d3542c6
kolyaklimk <kolyaklimk@gmail.com>
2023-03-26 20:44:52 +0300

    Create def top_K_repeated_N_grams

--- Lab2/Task1/Task1.py
+++ Lab2/Task1/Task1.py
@@ -37,13 +37,28 @@ def average_lenght_of_the_word(text):
     return count_char / count_words
 
 
+def top_K_repeated_N_grams(text: str, K=10, N=4):
+    n_grams: dict[str, int] = {}
+    words = re.findall(r'(?=\w).*?(?=\W)', text.lower())
+
+    for i in range(len(words) - N + 1):
+        n_gram = ' '.join(x for x in words[i:i + N])
+        if n_gram in n_grams:
+            n_grams[n_gram] += 1
+        else:
+            n_grams[n_gram] = 1
+
+    return sorted(n_grams.items(), reverse=True, key=lambda item: item[1])[:K]
+
+
 input_text = 'This is a text 123 a32 with some separators, and ' \
              'multiple sentences. Foo bar baz... ' \
              'Abbreviations can also appear a1b2c3 1234. ' \
              'Foo bar baz hello world hello world. Hi there. Hi... there. ' \
-             'How are you? I am fine! '
+             'How are you? I am fine! Foo bar baz hello.'
 
 print(amount_of_sentences(' ' + input_text + ' '))
 print(amount_of_non_declarative_sentences(' ' + input_text + ' '))
 print(average_lenght_of_the_sentence(' ' + input_text + ' '))
 print(average_lenght_of_the_word(' ' + input_text + ' '))
+print(top_K_repeated_N_grams(' ' + input_text + ' '))
