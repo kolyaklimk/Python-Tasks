1788738db2f02eb1b1474e0cd2fe655c79f48c0e
kolyaklimk <kolyaklimk@gmail.com>
2023-03-26 18:43:13 +0300

    Change unput_text for new def. Creat new def average_lenght_of_the_sentence

--- Lab2/Task1/Task1.py
+++ Lab2/Task1/Task1.py
@@ -9,11 +9,27 @@ def amount_of_non_declarative_sentences(text):
     return len(re.findall(r'[?!]\s', text))
 
 
-input_text = 'This is a text with some separators  and ' \
+def average_lenght_of_the_sentence(text):
+    sentences = re.findall(r'(?=\s[A-Z0-9]).*?(?=[.?!]\s)', text)
+    count_sentences = len(sentences)
+    count_char = 0
+
+    for sent in sentences:
+        words = re.findall(r'(?=\w).*?(?=\W)', sent + ' ')
+        for w in words:
+            if len(w) == len(re.findall(r'\d', w)):
+                continue
+            count_char += len(w)
+
+    return count_char / count_sentences
+
+
+input_text = 'This is a text 123 a32 with some separators, and ' \
              'multiple sentences. Foo bar baz... ' \
-             'abbreviations can also appear a1b2c3 1234. ' \
+             'Abbreviations can also appear a1b2c3 1234. ' \
              'Foo bar baz hello world hello world. Hi there. Hi... there. ' \
              'How are you? I am fine! '
 
-print(amount_of_sentences(input_text))
-print(amount_of_non_declarative_sentences(input_text))
+print(amount_of_sentences(' ' + input_text + ' '))
+print(amount_of_non_declarative_sentences(' ' + input_text + ' '))
+print(average_lenght_of_the_sentence(' ' + input_text + ' '))
