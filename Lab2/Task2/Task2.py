82236b50cddb26bd0760e3a2836a1f8ebd55496d
kolyaklimk <kolyaklimk@gmail.com>
2023-03-27 08:33:34 +0300

    Create def add

--- Lab2/Task2/Task2.py
+++ Lab2/Task2/Task2.py
@@ -1,3 +1,9 @@
 class MyContainer:
     def __init__(self):
         self.container = set()
+
+    def add(self, *keys):
+        for key in keys:
+            if key not in self.container:
+                self.container.add(key)
+   
