b8db8124a0bd325dae889b5e5ccff61973f9f1bd
kolyaklimk <kolyaklimk@gmail.com>
2023-03-27 10:32:15 +0300

    Init my_container and get method_list

--- Lab2/Task2/main.py
+++ Lab2/Task2/main.py
@@ -1,1 +1,6 @@
 import my_container
+
+if __name__ == "__main__":
+    username = input("Enter username: ")
+    container = my_container.MyContainer(username)
+    method_list = [method for method in dir(container) if method.startswith('__') is False]
