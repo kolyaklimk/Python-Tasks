a5f3aaf555b3ca4b94f3bc099b8bbc3f627d1119
kolyaklimk <kolyaklimk@gmail.com>
2023-03-27 10:38:44 +0300

    Append exit to method_list adn start While True

--- Lab2/Task2/main.py
+++ Lab2/Task2/main.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     username = input("Enter username: ")
     container = my_container.MyContainer(username)
     method_list = [method for method in dir(container) if method.startswith('__') is False]
+    method_list.append('exit')
+    
+    while True:
+        print(method_list)
