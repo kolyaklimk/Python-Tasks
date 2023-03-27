e844eb9619b4487a4a59d4544d01f240bd297a6d
kolyaklimk <kolyaklimk@gmail.com>
2023-03-27 11:20:11 +0300

    Fix input error

--- Lab2/Task2/main.py
+++ Lab2/Task2/main.py
@@ -14,29 +14,40 @@ if __name__ == "__main__":
         command = input(f'\nCommands: {method_list} \nEnter command: ')
         tokens = command.strip().split(' ')
 
-        if len(tokens) == 1 and tokens[0] != 'list':
-            print('Error! Write <command> <value...>')
-            continue
-
         match tokens[0]:
             case "add":
+                if len(tokens) == 1:
+                    print('Error! Write <command> <value...>')
+                    continue
                 container.add(tokens[1:])
             case "remove":
+                if len(tokens) == 1:
+                    print('Error! Write <command> <value...>')
+                    continue
                 container.remove(tokens[1])
             case "find":
-                container.remove(tokens[1:])
+                if len(tokens) == 1:
+                    print('Error! Write <command> <value...>')
+                    continue
+                container.remove(tokens[1])
             case "list":
                 container.list()
             case "grep":
+                if len(tokens) == 1:
+                    print('Error! Write <command> <value...>')
+                    continue
                 container.grep(tokens[1])
             case "save":
                 container.save()
             case "load":
                 container.load()
             case "switch":
+                if len(tokens) == 1:
+                    print('Error! Write <command> <value...>')
+                    continue
                 container.switch(tokens[1])
             case "exit":
-                if input(('Save', self.container, 'container?(y/n)')) == 'y':
+                if input(('Save', container.username, 'container?(y/n)')) == 'y':
                     container.save()
                 exit()
             case _:
