0ee98a7aa08421ae0e40c7e229123cad4cd362d8
kolyaklimk <kolyaklimk@gmail.com>
2023-05-16 00:18:03 +0300

    Add other options for from_dict

--- Lab3/MyParser/Parser.py
+++ Lab3/MyParser/Parser.py
@@ -116,3 +116,79 @@ class Parser:
 
         elif type(obj) is list:
             return [cls.from_dict(o) for o in obj]
+
+        else:
+            obj_type = obj[TYPE_KW]
+            obj_source = obj[SOURCE_KW]
+
+            if obj_type == dict.__name__:
+                return cls.from_dict(obj_source, is_dict=True)
+
+            cols_dict = {t.__name__: t for t in [set, frozenset, tuple, bytes, bytearray]}
+            if obj_type in cols_dict:
+                return cols_dict[obj_type](cls.from_dict(obj_source))
+
+            if obj_type == complex.__name__:
+                return obj_source[complex.real.__name__] + \
+                    obj_source[complex.imag.__name__] * 1j
+
+            if obj_type == moduletype.__name__:
+                return __import__(obj_source)
+
+            if obj_type == codetype.__name__:
+                return codetype(*[cls.from_dict(obj_source[prop]) for prop in CODE_PROPS])
+
+            if obj_type == celltype.__name__:
+                return celltype(cls.from_dict(obj_source))
+
+            if obj_type == smethodtype.__name__:
+                return staticmethod(cls.from_dict(obj_source))
+
+            if obj_type == cmethodtype.__name__:
+                return classmethod(cls.from_dict(obj_source))
+
+            if obj_type == functype.__name__:
+                code = cls.from_dict(obj_source[CODE_KW])
+                gvars = cls.from_dict(obj_source[GLOBALS_KW])
+                name = cls.from_dict(obj_source[NAME_KW])
+                defaults = cls.from_dict(obj_source[DEFAULTS_KW])
+                closure = cls.from_dict(obj_source[CLOSURE_KW])
+
+                for key in gvars:
+                    if key in code.co_name and key in globals():
+                        gvars[key] = globals()[key]
+
+                func = functype(code, gvars, name, defaults, closure)
+
+                if func.__name__ in gvars:
+                    func.__globals__.update({func.__name__: func})
+                return func
+
+            if obj_type == type.__name__:
+                name = cls.from_dict(obj_source[NAME_KW])
+                bases = cls.from_dict(obj_source[BASES_KW])
+                dct = obj_source[DICT_KW]
+                dct = {cls.from_dict(item[0]): cls.from_dict(item[1]) for item in dct.items()}
+
+                cl = type(name, bases, dct)
+
+                for attr in cl.__dict__.values():
+                    if inspect.isroutine(attr):
+                        if type(attr) in (smethodtype, classmethod):
+                            fglobs = attr.__func__.__globals__
+                        else:
+                            fglobs = attr.__globals__
+
+                        for gv in fglobs.keys():
+                            if gv == cl.__name__:
+                                fglobs[gv] = cl
+                return cl
+
+            else:
+                clas = cls.from_dict(obj_source[CLASS_KW])
+                dct = obj_source[DICT_KW]
+                dct = {cls.from_dict(item[0]): cls.from_dict(item[1]) for item in dct.items()}
+
+                o = object.__new__(clas)
+                o.__dict__ = dct
+                return o
