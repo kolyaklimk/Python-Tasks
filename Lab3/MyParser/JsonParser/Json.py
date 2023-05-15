30caf821bd8003f145aa08faa54ec8e34a11b43d
kolyaklimk <kolyaklimk@gmail.com>
2023-05-16 00:48:09 +0300

    Add simple options for json dumps

--- Lab3/MyParser/JsonParser/Json.py
+++ Lab3/MyParser/JsonParser/Json.py
@@ -7,9 +7,25 @@ from MyParser.JsonParser import TRUE_LITERAL, FALSE_LITERAL, NULL_LITERAL, INT_P
     STRING_PATTERN, NULL_PATTERN, VALUE_PATTERN
 
 
-class Json:
+class Json(Serializer):
     def dumps(self, obj) -> str:
-        pass
+        def dumps_from_dict(string) -> str:
+            if type(string) is nonetype:
+                return NULL_LITERAL
+            if type(string) is bool:
+                return TRUE_LITERAL if string else FALSE_LITERAL
+
+            if type(string) is str:
+                return '"' + string.replace('\\', "\\\\").replace('"', r"\"").replace("'", r"\'") + '"'
+
+            if type(string) in (int, float):
+                return str(string)
+
+            else:
+                raise ValueError
+
+        obj = Parser.to_dict(obj)
+        return dumps_from_dict(obj)
 
     def loads(self, obj: str):
         pass
