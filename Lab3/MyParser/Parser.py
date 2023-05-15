6563ebaebb6d6d9b91e71f0564a3c01ae720f086
kolyaklimk <kolyaklimk@gmail.com>
2023-05-16 00:07:57 +0300

    Add import to Parser

--- Lab3/MyParser/Parser.py
+++ Lab3/MyParser/Parser.py
@@ -1,4 +1,8 @@
 import inspect
+from MyParser import nonetype, moduletype, codetype, celltype, \
+    functype, smethodtype, cmethodtype, CODE_PROPS, UNIQUE_TYPES, \
+    TYPE_KW, SOURCE_KW, CODE_KW, GLOBALS_KW, NAME_KW, DEFAULTS_KW, CLOSURE_KW, \
+    BASES_KW, DICT_KW, CLASS_KW, OBJECT_KW
 
 
 class Parser:
