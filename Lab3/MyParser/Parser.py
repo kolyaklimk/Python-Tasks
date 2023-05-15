703bc3979541704c5b868ad83f7ef97808d45973
kolyaklimk <kolyaklimk@gmail.com>
2023-05-16 00:11:13 +0300

    Update to_dict method (add some options

--- Lab3/MyParser/Parser.py
+++ Lab3/MyParser/Parser.py
@@ -8,7 +8,48 @@ from MyParser import nonetype, moduletype, codetype, celltype, \
 class Parser:
     @classmethod
     def to_dict(cls, obj, is_inner_func=False):
-        pass
+        if type(obj) in (int, float, bool, str, nonetype):
+            return obj
+
+        if type(obj) in (set, frozenset, tuple, bytes, bytearray):
+            return {TYPE_KW: type(obj).__name__,
+                    SOURCE_KW: cls.to_dict([*obj])}
+
+        if type(obj) is complex:
+            return {TYPE_KW: complex.__name__,
+                    SOURCE_KW: {complex.real.__name__: obj.real, complex.imag.__name__: obj.imag}}
+
+        if type(obj) is list:
+            return [cls.to_dict(o) for o in obj]
+
+        if type(obj) is dict:
+            return {TYPE_KW: dict.__name__,
+                    SOURCE_KW: [[cls.to_dict(item[0]), cls.to_dict(item[1])] for item in obj.items()]}
+
+        if type(obj) is moduletype:
+            return {TYPE_KW: moduletype.__name__,
+                    SOURCE_KW: obj.__name__}
+
+        if type(obj) is codetype:
+            code = {TYPE_KW: codetype.__name__}
+            source = {}
+
+            for (key, value) in inspect.getmembers(obj):
+                if key in CODE_PROPS:
+                    source[key] = cls.to_dict(value)
+
+            code.update({SOURCE_KW: source})
+            return code
+
+        if type(obj) is celltype:
+            return {TYPE_KW: celltype.__name__,
+                    SOURCE_KW: cls.to_dict(obj.cell_contents)}
+
+        if type(obj) in (smethodtype, cmethodtype):
+            return {TYPE_KW: type(obj).__name__,
+                    SOURCE_KW: cls.to_dict(obj.__func__, is_inner_func)}
+
+        
 
     @classmethod
     def from_dict(cls, obj, is_dict=False):
