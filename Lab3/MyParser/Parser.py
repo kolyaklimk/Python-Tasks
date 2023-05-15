71c6ff2d77295fc643dd4f26d2da4e9fc08467ab
kolyaklimk <kolyaklimk@gmail.com>
2023-05-16 00:05:08 +0300

    Create Parser.py with 2 methods

---
+++ Lab3/MyParser/Parser.py
@@ -0,0 +1,11 @@
+import inspect
+
+
+class Parser:
+    @classmethod
+    def to_dict(cls, obj, is_inner_func=False):
+        pass
+
+    @classmethod
+    def from_dict(cls, obj, is_dict=False):
+        pass
