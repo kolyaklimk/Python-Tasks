e943dc57d9b1dd9ba4665d264bc36dc8e049f7af
kolyaklimk <kolyaklimk@gmail.com>
2023-05-16 00:33:04 +0300

    Init class json and add import

---
+++ Lab3/MyParser/JsonParser/Json.py
@@ -0,0 +1,14 @@
+import re
+import regex
+from MyParser.Parser import Parser
+from MyParser import nonetype
+from MyParser.JsonParser import TRUE_LITERAL, FALSE_LITERAL, NULL_LITERAL, INT_PATTERN, FLOAT_PATTERN, BOOL_PATTERN, \
+    STRING_PATTERN, NULL_PATTERN, VALUE_PATTERN
+
+
+class Json:
+    def dumps(self, obj) -> str:
+        pass
+
+    def loads(self, obj: str):
+        pass
