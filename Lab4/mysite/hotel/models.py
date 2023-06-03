1deca4223a7176442887722453bd92eab4fd10b3
Kolya <kolyaklimk@gmail.com>
2023-06-03 19:08:08 +0300

    Update models.py

--- Lab4/mysite/hotel/models.py
+++ Lab4/mysite/hotel/models.py
@@ -3,55 +3,38 @@ from django.db import models
 
 # https://django.fun/ru/docs/django/4.1/topics/db/models/
 # https://django.fun/ru/docs/django/4.1/ref/models/fields/#model-field-types
-class TypeRoom(models.Model):
-    name = models.CharField(max_length=100)
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Room(models.Model):
-    type = models.ForeignKey(TypeRoom, on_delete=models.CASCADE)
-    capacity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    capacity = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    description = models.TextField()
-    photo = models.ImageField(upload_to='photos/')
+    photo = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
 
     def __str__(self):
-        return f"Room {self.id}"
-
-
-class Payment(models.Model):
-    type = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.type
+        return f"Room {self.id} ({self.category})"
 
 
 class Client(models.Model):
-    last_name = models.CharField(max_length=100, blank=True)
-    name = models.CharField(max_length=100, blank=True)
-    patronymic = models.CharField(max_length=100, blank=True)
-    commentary = models.TextField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
     has_child = models.BooleanField(blank=True)
-    payment_type = models.ManyToManyField(Payment, blank=True)
 
     def __str__(self):
-        return f"{self.last_name} {self.name}"
+        return f"{self.last_name} {self.first_name} {self.patronymic}"
 
 
-class Period(models.Model):
-    check_in_date = models.DateField(blank=True)
-    eviction_date = models.DateField(blank=True)
-
-    def __str__(self):
-        return f"Check-in: {self.check_in_date}; Eviction: {self.eviction_date}"
-
-
-class Reservation(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+class Booking(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    residence_period = models.OneToOneField(Period, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
 
     def __str__(self):
-        return f"Reservation {self.id}"
+        return f"Booking {self.id} - {self.client} ({self.room})"
