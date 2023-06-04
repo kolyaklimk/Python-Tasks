c3d22dfb62a46ec178aafa449e11805bee570622
Kolya <kolyaklimk@gmail.com>
2023-06-04 11:15:26 +0300

    Update form for lab

--- Lab4/mysite/auth_user/forms.py
+++ Lab4/mysite/auth_user/forms.py
@@ -1,19 +1,48 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
+
+from auth_user.views import validate_age
+from hotel.models import Client
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    patronymic = forms.CharField(label='Patronymic', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Password replay', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
+    birth_date = forms.DateField(
+        widget=forms.DateInput(format='%d/%m/%Y', attrs={'placeholder': 'DD/MM/YYYY', 'class': 'form-control'}),
+        label='Birth date', validators=[validate_age])
+
+    phone_number = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '+375 (XX) XXX-XX-XX', 'class': 'form-input'}),
+        max_length=20,
+        required=False,
+        help_text='Format: +375 (XX) XXX-XX-XX',
+        label='Phone number',
+        validators=[RegexValidator(regex=r'^\+375 \(\d{2}\) \d{3}-\d{2}-\d{2}$',
+                                   message='Phone number must be in the format +375 (XX) XXX-XX-XX')])
+
+    class Meta(UserCreationForm.Meta):
+        model = Client
+        fields = ('first_name',
+                  'last_name',
+                  'patronymic',
+                  'birth_date',
+                  'phone_number',
+                  'username',
+                  'email',
+                  'password1',
+                  'password2',)
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
