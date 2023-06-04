686b90438f4381958870d4aff6dcadbd1f3e1552
Kolya <kolyaklimk@gmail.com>
2023-06-04 11:16:07 +0300

    Update Client and Booking

--- Lab4/mysite/hotel/models.py
+++ Lab4/mysite/hotel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # https://django.fun/ru/docs/django/4.1/topics/db/models/
@@ -14,17 +15,18 @@ class Room(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     capacity = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    photo = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    photo = models.ImageField(upload_to='product/', blank=True)
 
     def __str__(self):
         return f"Room {self.id} ({self.category})"
 
 
-class Client(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    patronymic = models.CharField(max_length=50)
-    has_child = models.BooleanField(blank=True)
+class Client(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    phone_number = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.patronymic}"
@@ -35,6 +37,7 @@ class Booking(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     check_in_date = models.DateField()
     check_out_date = models.DateField()
+    has_child = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Booking {self.id} - {self.client} ({self.room})"
