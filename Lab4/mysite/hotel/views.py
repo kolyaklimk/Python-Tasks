acf113168dac040ad42b456618c58f786a8dbfd2
Kolya <kolyaklimk@gmail.com>
2023-06-04 22:57:37 +0300

    Update views.py

--- Lab4/mysite/hotel/views.py
+++ Lab4/mysite/hotel/views.py
@@ -1,21 +1,28 @@
 from datetime import datetime
 
-from django.shortcuts import render
-from .models import Room, Category, Client, Booking
+from django.shortcuts import render, redirect
+from .models import Room, Category, Booking
 from django.shortcuts import get_object_or_404
-from django import forms
 
 
 def room_list(request, category_id):
-    if category_id == 0:
-        rooms = Room.objects.all()
-    else:
-        rooms = Room.objects.filter(category_id=category_id)
+    def get_html():
+        if category_id == 0:
+            rooms = Room.objects.all()
+        else:
+            rooms = Room.objects.filter(category_id=category_id)
 
-    categories = Category.objects.all()
-    return render(request, 'hotel/hotel_list.html', {'rooms': rooms,
-                                                     'categories': categories,
-                                                     'choose_category': category_id})
+        categories = Category.objects.all()
+        return render(request, 'hotel/hotel_list.html', {'rooms': rooms,
+                                                         'categories': categories,
+                                                         'choose_category': category_id})
+
+    if request.method == 'POST' and 'delete_room' in request.POST:
+        room = get_object_or_404(Room, id=request.POST['room_id'])
+        room.delete()
+        return get_html()
+
+    return get_html()
 
 
 def sort_rooms_by_price(request, category_id):
@@ -32,8 +39,6 @@ def sort_rooms_by_price(request, category_id):
 
 
 def room_detail(request, room_id):
-    global periods
-
     def check_date_periods(start1, end1, start2, end2):
         if start1 < end2 and start2 < end1:
             return True
@@ -41,8 +46,9 @@ def room_detail(request, room_id):
             return False
 
     form = get_object_or_404(Room, id=room_id)
+    categories = Category.objects.all()
 
-    if request.method == 'POST':
+    if request.method == 'POST' and 'book_room' in request.POST:
         check_in_date = datetime.strptime(request.POST['check_in_date'], '%Y-%m-%d').date()
         check_out_date = datetime.strptime(request.POST['check_out_date'], '%Y-%m-%d').date()
         has_child = request.POST.get('my_checkbox', False)
@@ -69,27 +75,42 @@ def room_detail(request, room_id):
             has_child=True if has_child == "on" else False
         )
         booking.save()
-        return room_list(request, 0)
+        return redirect("hotel:hotel_list", 0)
 
-    return render(request, 'hotel/room_detail.html', {'form': form})
+    if request.method == 'POST' and 'edit_room' in request.POST:
+        price = request.POST['price']
+        capacity = request.POST['capacity']
+        select = request.POST['select']
 
-# def create_room(request):
-#     form = RoomForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         return redirect('room_list')
-#     return render(request, 'room_form.html', {'form': form})
-#
-#
-# def update_room(request, room_id):
-#     room = Room.objects.get(id=room_id)
-#     form = RoomForm(request.POST or None, instance=room)
-#     if form.is_valid():
-#         form.save()
-#         return redirect('room_list')
-#     return render(request, 'room_form.html', {'form': form})
-#
-#
-# def delete_room(request, room_id):
-#     product = Room.objects.get(id=id)
-#     product.delete()
+        category = get_object_or_404(Category, id=select)
+        form.category = category
+        form.capacity = capacity
+        form.price = price
+
+        form.save()
+        form = get_object_or_404(Room, id=room_id)
+
+    return render(request, 'hotel/room_detail.html', {'form': form, 'categories': categories})
+
+
+def create_room(request):
+    if not request.user.is_authenticated:
+        return redirect("hotel:hotel_list", 0)
+
+    categories = Category.objects.all()
+    if request.method == 'POST' and 'create_room' in request.POST:
+        price = request.POST['price']
+        capacity = request.POST['capacity']
+        select = request.POST['select']
+
+        category = get_object_or_404(Category, id=select)
+
+        new_room = Room()
+        new_room.category = category
+        new_room.photo = '1'
+        new_room.capacity = capacity
+        new_room.price = price
+        new_room.save()
+        return redirect("hotel:hotel_list", 0)
+
+    return render(request, 'hotel/room_create.html', {'categories': categories})
