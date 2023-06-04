d0a2b9b5a9b4a1a9c560e268b5e7b816ddeb46cd
Kolya <kolyaklimk@gmail.com>
2023-06-04 21:35:20 +0300

    Update views.py

--- Lab4/mysite/profile_user/views.py
+++ Lab4/mysite/profile_user/views.py
@@ -9,17 +9,18 @@ def get_book(request):
         return redirect("hotel:hotel_list", 0)
 
     def get_booking():
-        return Booking.objects.filter(client_id=request.user, status='cart')
+        all_booking = Booking.objects.filter(client_id=request.user, status='cart')
+        return render(request, 'profile_user/profile.html', {'all_booking': all_booking})
 
     if request.method == 'POST' and 'delete_item' in request.POST:
         booking = get_object_or_404(Booking, id=request.POST['booking_id'])
         booking.delete()
-        return render(request, 'profile_user/profile.html', {'all_booking': get_booking()})
+        return get_booking()
 
     if request.method == 'POST' and 'confirm' in request.POST:
         for i in Booking.objects.filter(client_id=request.user, status='cart'):
             i.status = 'paid'
             i.save()
-        return render(request, 'profile_user/profile.html', {'all_booking': get_booking()})
+        return get_booking()
 
-    return render(request, 'profile_user/profile.html', {'all_booking': get_booking()})
+    return get_booking()
