c80a8aa70db78812caa22cc5b884d47b20efc219
Kolya <kolyaklimk@gmail.com>
2023-06-04 18:32:35 +0300

    Add room_detail with "book a room"

--- Lab4/mysite/hotel/views.py
+++ Lab4/mysite/hotel/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
-from .models import Room, Client, Booking, Category
+from datetime import datetime
+
+from django.shortcuts import render
+from .models import Room, Category, Client, Booking
 from django.shortcuts import get_object_or_404
+from django import forms
 
 
 def room_list(request, category_id):
@@ -29,7 +32,45 @@ def sort_rooms_by_price(request, category_id):
 
 
 def room_detail(request, room_id):
+    global periods
+
+    def check_date_periods(start1, end1, start2, end2):
+        if start1 < end2 and start2 < end1:
+            return True
+        else:
+            return False
+
     form = get_object_or_404(Room, id=room_id)
+
+    if request.method == 'POST':
+        check_in_date = datetime.strptime(request.POST['check_in_date'], '%Y-%m-%d').date()
+        check_out_date = datetime.strptime(request.POST['check_out_date'], '%Y-%m-%d').date()
+        has_child = request.POST.get('my_checkbox', False)
+        if check_in_date > check_out_date:
+            error_message = "Check in date < Check out date!"
+            return render(request, 'hotel/room_detail.html', {'error_message': error_message})
+
+        periods = []
+
+        all_booking = Booking.objects.filter(room_id=room_id)
+        for i in all_booking:
+            if check_date_periods(check_in_date, check_out_date, i.check_in_date, i.check_out_date):
+                periods.append(str(i.check_in_date) + ' --> ' + str(i.check_out_date))
+
+        if len(periods):
+            error_message = "Already booked these days: " + str(periods) + '!'
+            return render(request, 'hotel/room_detail.html', {'error_message': error_message})
+
+        booking = Booking(
+            room=form,
+            client=request.user,
+            check_in_date=check_in_date,
+            check_out_date=check_out_date,
+            has_child=True if has_child == "on" else False
+        )
+        booking.save()
+        return room_list(request, 0)
+
     return render(request, 'hotel/room_detail.html', {'form': form})
 
 # def create_room(request):
