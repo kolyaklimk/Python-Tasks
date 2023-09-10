c5935736551fdc2d8ab7f8519c793189552d611c
Kolya <kolyaklimk@gmail.com>
2023-09-10 23:52:44 +0300

    Update news

--- 4th semester university/Lab4/mysite/news/urls.py
+++ 4th semester university/Lab4/mysite/news/urls.py
@@ -5,4 +5,5 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.news_list, name='home'),
+    path('<int:news_id>/', views.current_news, name='current_news'),
 ]
--- 4th semester university/Lab4/mysite/news/views.py
+++ 4th semester university/Lab4/mysite/news/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News
 
 
 def news_list(request):
-    # news_list2 = News.objects.all()
-    # context = {
-    #     'news_list': news_list2,
-    # }
-    # return render(request, 'news/news_list.html', context)
-    return render(request, '')
+    news_list = News.objects.order_by('-publication_date')
+    context = {
+        'news_list': news_list,
+    }
+    return render(request, 'news.html', context)
+
+def current_news(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    context = {
+        'news': news,
+    }
+    return render(request, 'current_news.html', context)
