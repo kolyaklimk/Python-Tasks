7d359fec6a9d6a54b15d47552bb18dcaaea3fbdb
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-10-16 13:54:56 +0300

    Update

--- 4th semester university/Lab4/mysite/auth_user/forms.py
+++ 4th semester university/Lab4/mysite/auth_user/forms.py
@@ -9,20 +9,20 @@ from hotel.models import Client
 
 
 class RegisterUserForm(UserCreationForm):
-    first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    patronymic = forms.CharField(label='Patronymic', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Password replay', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'input-1'}))
+    last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'input-1'}))
+    patronymic = forms.CharField(label='Patronymic', widget=forms.TextInput(attrs={'class': 'input-1'}))
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'input-1'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'input-1'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'input-1'}))
+    password2 = forms.CharField(label='Password replay', widget=forms.PasswordInput(attrs={'class': 'input-1'}))
 
     birth_date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'input-1'}),
         label='Birth date', validators=[validate_age])
 
     phone_number = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': '+375 (XX) XXX-XX-XX', 'class': 'form-input'}),
+        widget=forms.TextInput(attrs={'placeholder': '+375 (XX) XXX-XX-XX', 'class': 'input-1'}),
         max_length=20,
         required=False,
         help_text='Format: +375 (XX) XXX-XX-XX',
@@ -44,5 +44,5 @@ class RegisterUserForm(UserCreationForm):
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'input-1'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'input-1'}))
